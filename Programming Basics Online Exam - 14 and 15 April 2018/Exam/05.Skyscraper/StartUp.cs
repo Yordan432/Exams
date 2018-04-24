19bd249154131c88cd8ba9e70f3377127f865d29
Yordan432 <dani.583@abv.bg>
2018-04-24 20:38:42 +0300

    Author's decision of task Skycraper

--- Programming Basics Online Exam - 14 and 15 April 2018/Exam/05.Skyscraper/StartUp.cs
+++ Programming Basics Online Exam - 14 and 15 April 2018/Exam/05.Skyscraper/StartUp.cs
@@ -5,18 +5,19 @@ class StartUp
     {
         int size = int.Parse(Console.ReadLine());
 
+        int countSpace = size;
         int count = size;
         for (int i = 0; i < size / 4; i++)
         {
-            Console.Write(new string(' ', size));
+            Console.Write(new string(' ', countSpace));
             Console.WriteLine("|");
         }
+        countSpace--;
         Console.Write(new string(' ', size - 1));
         Console.Write("_");
         Console.Write("|");
         Console.WriteLine("_");
-
-        int specialRow = 1;
+        
         for (int i = 0; i < size / 4; i++)
         {
             Console.Write(new string(' ', size - 1));
@@ -30,7 +31,6 @@ class StartUp
         Console.Write("-");
         Console.Write("|");
         Console.WriteLine("_");
-
         for (int i = 0; i < size / 4; i++)
         {
             Console.Write(new string(' ', size - 2));
@@ -38,12 +38,47 @@ class StartUp
             Console.Write(new string('*', 3));
             Console.WriteLine("|");
         }
-        Console.Write(new string(' ', size - 3));
+        countSpace--;
+        if (size % 2 == 0)
+        {
+            Console.Write(new string(' ', size - 3));
+            Console.Write("_");
+            Console.Write("|");
+            Console.Write(new string('*', 3));
+            Console.Write("|");
+            Console.WriteLine("_");
+            countSpace--;
+        }
+        else
+        {
+            Console.Write(new string(' ', size - 2));
+            Console.Write("|");
+            Console.Write(new string('*', 3));
+            Console.WriteLine("|");
+        }
+        // n * 2 - 1
+        string isEvenNumber = size % 2 == 0 ? "|" : "";
+        for (int i = 0; i < size * 4 - 1; i++)
+        {
+            Console.Write(new string(' ', countSpace));
+            Console.Write(isEvenNumber);
+            Console.Write("|");
+            Console.Write(new string('-' , 3));
+            Console.Write("|");
+            Console.WriteLine(isEvenNumber); 
+        }
+        Console.Write(new string(' ', countSpace - 1));
         Console.Write("_");
+        Console.Write(isEvenNumber);
         Console.Write("|");
-        Console.Write(new string('*', 3));
+        Console.Write(new string('-', 3));
         Console.Write("|");
+        Console.Write(isEvenNumber);
         Console.WriteLine("_");
+
+        for (int i = 0; i < size - 2; i++)
+        {
+            Console.WriteLine(new string('|' , size * 2 + 1));
+        }
     }
 }
-
