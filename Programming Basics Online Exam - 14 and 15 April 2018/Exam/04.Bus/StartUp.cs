aa9a3471045348dda9a04054ac5fcdab6bb9b149
Yordan432 <dani.583@abv.bg>
2018-04-24 17:12:59 +0300

    Revert "Revert "Exam from 14-15 April 2018""
    This reverts commit bf1400631312ce5f34f3241ff20f687701cb4d75.

---
+++ Programming Basics Online Exam - 14 and 15 April 2018/Exam/04.Bus/StartUp.cs
@@ -0,0 +1,32 @@
+﻿using System;
+class StartUp
+{
+    static void Main()
+    {
+        int numberOfPerson = int.Parse(Console.ReadLine());
+        int numberStop = int.Parse(Console.ReadLine());
+
+        int checkPerson = 1;
+        int totalTrainPerson = numberOfPerson;
+        for (int i = 0; i < numberStop; i++)
+        {
+            int outBus = int.Parse(Console.ReadLine());
+            int inBus = int.Parse(Console.ReadLine());
+
+            totalTrainPerson -= outBus;
+            totalTrainPerson += inBus;
+
+            if (checkPerson % 2 == 1)
+            {
+                totalTrainPerson += 2;
+            }
+            else
+            {
+                totalTrainPerson -= 2;
+            }
+            checkPerson++;
+        }
+        Console.WriteLine($"The final number of passengers is : {totalTrainPerson}");
+    }
+}
+
