f41ff4271631cefd597c5abfa7e23000362bb4fc
Yordan432 <dani.583@abv.bg>
2018-05-31 21:17:03 +0300

    exam from 28 29 April Basics

---
+++ Programming Basics Online Exam - 28 and 29 April 2018/Exam 28 29 April/01.Programming Book/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+class ProgrammingBook
+{
+    static void Main()
+    {
+        double pricePage = double.Parse(Console.ReadLine());
+        double priceOfCover = double.Parse(Console.ReadLine());
+        int PercentReductionPrintingPaper = int.Parse(Console.ReadLine());
+        double sumForPay = double.Parse(Console.ReadLine());
+        int profitOfTotalSum = int.Parse(Console.ReadLine());
+
+        int numberOfPages = 899;
+        int convers = 2;
+
+        double reduction = pricePage * numberOfPages + priceOfCover * convers;
+        double profitReduction = reduction - (reduction * (PercentReductionPrintingPaper / 100.0));
+
+        double moneyForDesigner = profitReduction + sumForPay;
+        double profitTeam = moneyForDesigner - (moneyForDesigner * (profitOfTotalSum / 100.0));
+
+        Console.WriteLine($"Avtonom should pay {profitTeam:f2} BGN.");
+    }
+}
+
---
+++ Programming Basics Online Exam - 28 and 29 April 2018/Exam 28 29 April/01.Sea Trip/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        double moneyForFood = double.Parse(Console.ReadLine());
+        double moneyForSouvenirs = double.Parse(Console.ReadLine());
+        double moneyForHotel = double.Parse(Console.ReadLine());
+
+        double roadToSea = 210;
+        double totalRoadKm = roadToSea * 2;
+
+        double litersGasoline = 7;
+        double killometers = 100;
+        double priceGasoline = 1.85;
+        int numberOfDay = 3;
+
+        double literGasoline = (totalRoadKm / killometers) * litersGasoline;
+        double gasoline = literGasoline * priceGasoline;
+
+        double cost = numberOfDay * moneyForFood + numberOfDay * moneyForSouvenirs;
+
+        double firstDayReduction = moneyForHotel * 0.9;
+        double secondDayReduction = moneyForHotel * 0.85;
+        double thirdDayReduction = moneyForHotel * 0.8;
+
+        double totalSum = gasoline + cost + firstDayReduction +
+            secondDayReduction + thirdDayReduction;
+
+        Console.WriteLine($"Money needed: {totalSum}");
+    }
+}
+
